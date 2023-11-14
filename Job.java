bd3228bdd874e771022c8a93cbbdccb862f9f40d
Ibrahimyousuf22 <92476951+Ronaldodjd@users.noreply.github.com>
2023-11-14 17:27:12 -0500

    Ibrahim's commit

---
+++ Job.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Job {
+
+    // Variables
+    public static AtomicInteger count = new AtomicInteger(0);
+    public static int numberOfThreads = 100;
+
+    public static void main(String[] args) {
+
+        // Create a list of Callables
+        List<Callable<Integer>> callables = new ArrayList<>();
+        for (int i = 0; i < numberOfThreads; i++) {
+            callables.add(new MyCallable());
+        }
+
+        // Create an ExecutorService with a fixed thread pool
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        try {
+            // Submit tasks to the executor and get a list of Futures
+            List<Future<Integer>> futures = executorService.invokeAll(callables);
+
+            // Wait for all tasks to complete and accumulate the results
+            for (Future<Integer> future : futures) {
+                count.addAndGet(future.get());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // Shutdown the executor service
+            executorService.shutdown();
+        }
+
+        System.out.println(count);
+    }
+}
+
+// Counting Program
+class MyCallable implements Callable<Integer> {
+    @Override
+    public Integer call() {
+        int localCount = 0;
+        for (int x = 0; x < 1_000_000; x++) {
+            localCount++;
+        }
+        return localCount;
+    }
+}
